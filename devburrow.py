f19b68fdd42cc3e1e073149bc68c2f85235506af
Walt Horat <walt@horat.net>
2022-09-25 18:53:22 -0700

    add more podcasters to dev burrow

--- devburrow.py
+++ devburrow.py
@@ -30,9 +30,15 @@ def buildBurrow():
 	loadProgramId(output, "Vaccines Exposed", 61701)
 
 	#Alphabetical podcasters
+	loadProgramId(output, "BradCGZ", 62937)
 	loadProgramId(output, "IPOT1776", 60413)
 	loadProgramId(output, "M3thods", 59991)
-	loadProgramId(output, "BradCGZ", 62937)
+	loadProgramId(output, "Mouthy Buddha", 60411)
+	loadProgramId(output, "Patel Patriot", 67973)
+	loadProgramId(output, "Quite Frankly", 67997)
+	loadProgramId(output, "RedPill78", 67996)
+	loadProgramId(output, "Tru Reporting", 67998)
+	loadProgramId(output, "Woke Societies", 67999)
 
 	#Alphabetical categories
 	loadProgramId(output, "5G", 59660)
@@ -74,7 +80,6 @@ def buildBurrow():
 	loadProgramId(output, "Marrs, Jim", 60414)
 	loadProgramId(output, "Meyer, Stanley", 60415)
 	loadProgramId(output, "Mind Control", 60416)
-	loadProgramId(output, "Mouthy Buddha", 60411)
 	loadProgramId(output, "Music", 61758)
 	loadProgramId(output, "Mystery Babylon Radio", 60418)
 	loadProgramId(output, "NASA", 60419)
