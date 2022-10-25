44808d3ffebae6b87d63e94c3d962f96758ca2be
Walt Horat <walt@horat.net>
2022-10-25 12:33:19 -0700

    update colony dev

--- devcolony.py
+++ devcolony.py
@@ -6,35 +6,38 @@ def buildBurrow():
 	#prepareAllCategory()
 
 	#Featured content
-	loadProgramId(output, "Featured", 58493)
-	loadProgramId(output, "Just Added", 58474)
-	reserveTopChronological(output, "Just Added")
+	loadProgramId(output, "Burrow: Featured", 58493)
+	loadProgramId(output, "Burrow: Just Added", 58474)
+	#reserveTopChronological(output, "Latest Podcasts")
 
 	#Alphabetical podcasters
-	loadCategory(output, "Amazing Polly", "amazing-polly")		# √
-	loadProgramId(output, "BradCGZ", 62937)						# √
-	loadCategory(output, "Corbett Report", "corbett-report")	# √
-	loadCategory(output, "Dark Journalist", "dark-journalist")
-	loadCategory(output, "Destroying the Illusion", "destroying-the-illusion")
-	loadCategory(output, "Dustin Nemos", "dustin-nemos")
-	loadCategory(output, "IPOT1776", "ipot1776")				# √
-	loadCategory(output, "L", "l")	
+	loadCategory(output, "Amazing Polly", "amazing-polly")		# √ 32
+	loadProgramId(output, "BradCGZ", 62937)
+	loadCategory(output, "Corbett Report", "corbett-report")	# √ 18
+	loadCategory(output, "Dark Journalist", "dark-journalist")	# √ 23
+	loadCategory(output, "Dustin Nemos", "dustin-nemos")		# √ 23
+	loadCategory(output, "Jordan Sather", "jordan-sather")		# 18
+	loadCategory(output, "Kate Awakening", "kate-awakening")	# 
+	loadCategory(output, "L", "l")								# 16
 	loadProgramId(output, "M3thods", 59991)						# √
-	loadProgramId(output, "Mouthy Buddha", 60411)				# √
-	loadCategory(output, "Patel Patriot", "patel-patriot")		# √
-	loadCategory(output, "Praying Medic", "praying-medic")
-	loadCategory(output, "Quite Frankly", "quite-frankly")		# √
-	loadCategory(output, "RedPill78", "redpill78")				# √
-	loadCategory(output, "Really Graceful", "really-graceful")	# √
-	loadCategory(output, "Sarah Westall", "sarah-westall")
-	loadCategory(output, "SGT Report", "sgt-report")
-	loadCategory(output, "TRUreporting", "trureporting")
-	loadCategory(output, "Woke Societies", "woke-societies")
+	loadCategory(output, "Patel Patriot", "patel-patriot")		# 16
+	loadCategory(output, "Praying Medic", "praying-medic")		#
+	loadCategory(output, "Quite Frankly", "quite-frankly")		# 17
+	loadCategory(output, "Really Graceful", "really-graceful")	# 26
+	loadCategory(output, "Redpill78", "redpill78")				# 17
+	loadCategory(output, "Robert Sepehr", "robert-sepehr")		# √
+	loadCategory(output, "Sarah Westall", "sarah-westall")		#
+	loadCategory(output, "SGT Report", "sgt-report")			# 20
+	loadCategory(output, "Tore Says", "tore-says")				# 
+	loadCategory(output, "TRUreporting", "trureporting")		# √ 18
+	loadCategory(output, "Woke Societies", "woke-societies")	# 
 
-	fillTopChronological(output, 100)	# up to 100 videos max
+	#fillTopChronological(output, 100)	# up to 100 videos max
 
 	writeOutput(output, "/var/www/html/AVideo/rokuv2/devcolony.json")
 
 # Do it. Build the channel.
 buildBurrow()
 
+	loadProgramId(output, "BradCGZ", 62937)						# √ 30+
+	loadProgramId(output, "Mouthy Buddha", 60411)				# √
