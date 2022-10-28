7bd1f0aa3f8eb8b44b2d34d764427a6a6e8132b7
Walt Horat <walt@horat.net>
2022-10-28 01:29:42 -0700

    fix

--- buildFeaturedPlaylist.py
+++ buildFeaturedPlaylist.py
@@ -2,10 +2,16 @@ from RokuFunctions import *
 
 def buildFeaturedPlaylist():
 	output = createAntMediaPlaylist("24/7 loop")
-	buildPlaylist(output, "https://conspyre.tv/roku.json?program_id=58493",	# videos
-					  "https://adserver3.conspyre.tv/roku.json?program_id=129", # commercials
-					  "https://adserver.conspyre.tv/roku.json?program_id=109", # bumpers
-					  "https://adserver.conspyre.tv/roku.json?program_id=110") # memes
+	
+	# buildPlaylist(output, "https://conspyre.tv/roku.json?program_id=58493",	# videos
+	# 				  "https://adserver3.conspyre.tv/roku.json?program_id=129", # commercials
+	# 				  "https://adserver.conspyre.tv/roku.json?program_id=109", # bumpers
+	# 				  "https://adserver.conspyre.tv/roku.json?program_id=110") # memes
+	
+	buildPlaylist(output, "https://conspyre.tv/roku.json?program_id=58493",
+					  "https://pepe.conspyre.tv/rokuv2/commercials_conspyre.json",
+					  "https://pepe.conspyre.tv/rokuv2/bumpers.json",
+					  "https://pepe.conspyre.tv/rokuv2/memes.json")
 	writeAntMediaJSON(output, "/var/www/html/AVideo/rokuv2/featured_antmedia.json")
 
 # Do it. Build the playlist.
